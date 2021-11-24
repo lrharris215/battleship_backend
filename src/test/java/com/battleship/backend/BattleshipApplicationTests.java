8d1d6f9d942124cb07f19a76fa3fa6397e9a6a87
Lane Harris <lrharris215@gmail.com>
2021-11-24 12:40:26 -0500

    Add testTest to make sure gradle and spring are working correctly

--- src/test/java/com/battleship/backend/BattleshipApplicationTests.java
+++ src/test/java/com/battleship/backend/BattleshipApplicationTests.java
@@ -2,12 +2,19 @@ package com.battleship.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BattleshipApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testTest(){
+		int five = 5;
+		assertEquals(8, five + 3);
 	}
 
 }
