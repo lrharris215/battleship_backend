7e6640f9cdd145f8b2f4fed471fd30628a3473b8
Lane Harris <lrharris215@gmail.com>
2021-11-24 14:33:38 -0500

    Reorganize: Move WelcomeController
    Move WelcomeController to a Controllers package for organization purposes

--- src/main/java/com/battleship/backend/controllers/WelcomeController.java
+++ src/main/java/com/battleship/backend/controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.battleship.backend;
+package com.battleship.backend.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/com/battleship/backend/BattleshipApplicationTests.java
+++ src/test/java/com/battleship/backend/BattleshipApplicationTests.java
@@ -1,5 +1,6 @@
 package com.battleship.backend;
 
+import com.battleship.backend.controllers.WelcomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
