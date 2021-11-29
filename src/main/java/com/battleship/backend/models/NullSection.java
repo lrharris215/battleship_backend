85650bfc3f3e658ce26da7c5639b802604b01023
Lane Harris <lrharris215@gmail.com>
2021-11-29 16:18:50 -0500

    Add: NullSection to represent empty ocean tiles
    NullSection implements Sectionable, which will allow it to respond to receiveHit and getIsHit. getShip will return null.

---
+++ src/main/java/com/battleship/backend/models/NullSection.java
@@ -0,0 +1,21 @@
+package com.battleship.backend.models;
+
+public class NullSection implements Sectionable{
+    boolean isHit;
+
+    public NullSection(){
+        isHit = false;
+    }
+
+    public void receiveHit() {
+        isHit = true;
+    }
+
+    public boolean getIsHit(){
+        return isHit;
+    }
+
+    public String getShip(){
+        return null;
+    }
+}
--- src/main/java/com/battleship/backend/models/Sectionable.java
+++ src/main/java/com/battleship/backend/models/Sectionable.java
@@ -3,5 +3,9 @@ package com.battleship.backend.models;
 public interface Sectionable {
 
     void receiveHit();
+
+    boolean getIsHit();
+
+    String getShip();
 }
 
