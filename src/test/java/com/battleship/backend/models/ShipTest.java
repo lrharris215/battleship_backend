fe30356d59dc5ebbc60e1924210ec147f353297b
Lane Harris <lrharris215@gmail.com>
2021-11-29 11:43:54 -0500

    Add: Test for Ship#rotate, #setHeight, #setWidth

--- src/test/java/com/battleship/backend/models/ShipTest.java
+++ src/test/java/com/battleship/backend/models/ShipTest.java
@@ -1,32 +1,48 @@
 package com.battleship.backend.models;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class ShipTest {
-
+    Ship testShip;
+    @BeforeEach
+    void initTestShip(){
+        testShip = new Ship("Test", 3);
+    }
     @Test
-    void fillShipPlacesShipSections() {
-        Ship testShip = new Ship("Test", 3);
-
+    void testFillShipPlacesShipSections() {
         assertNotNull(testShip.getShipSections());
     }
 
     @Test
-    void rotate() {
+    void testRotate() {
+        int width = testShip.getWidth();
+        testShip.rotate();
+
+        assertEquals(testShip.getHeight(), width);
     }
 
     @Test
     void checkIsSunk() {
+
     }
 
     @Test
-    void setWidth() {
+    void testSetWidthSetsTheWidth() {
+        int newWidth = 4;
+        testShip.setWidth(newWidth);
+
+        assertEquals(newWidth, testShip.getWidth());
     }
 
     @Test
-    void setHeight() {
+    void testSetHeightSetsTheHeight() {
+        int newHeight = 4;
+        testShip.setHeight(newHeight);
+
+        assertEquals(newHeight, testShip.getHeight());
     }
 }
