750294dd28766d7c17bd51faccf72951798189eb
Lane Harris <lrharris215@gmail.com>
2021-11-29 16:28:07 -0500

    Add: Board#setUpBoard
    Board now initializes with all the spaces in the grid filled with NullSections.

--- src/main/java/com/battleship/backend/models/Board.java
+++ src/main/java/com/battleship/backend/models/Board.java
@@ -7,6 +7,8 @@ public class Board {
     public Board(String name){
         this.name = name;
         grid = new Sectionable[10][10];
+
+        setUpBoard();
     }
 
     public Sectionable[][] getGrid(){
@@ -14,7 +16,11 @@ public class Board {
     }
 
     private void setUpBoard() {
-
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                grid[i][j] = new NullSection();
+            }
+        }
     }
 
 
