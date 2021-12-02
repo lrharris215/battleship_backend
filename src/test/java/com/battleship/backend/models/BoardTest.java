617e91d05c26262d6ab32e5e16decef1638703c9
Lane Harris <lrharris215@gmail.com>
2021-12-02 14:13:30 -0500

    Test: Add tests for all getters and setters in Board

--- src/test/java/com/battleship/backend/models/BoardTest.java
+++ src/test/java/com/battleship/backend/models/BoardTest.java
@@ -1,28 +1,27 @@
 package com.battleship.backend.models;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+    Board testBoard;
+    Ship testShip;
+    @BeforeEach
+    void initTestBoardandShip(){
+        testBoard = new Board("TestBoard");
+        testShip = new Ship("TestShip", 3);
+    }
     @Test
     public void testAddShipsPlacesFirstShipSectionOnBoard(){
-        Board testBoard = new Board("test");
-        Ship testShip = new Ship("testShip", 3);
-
-        // ship, x-coord, y-coord
         testBoard.addShip(testShip, 0, 0 );
 
         assertEquals(testBoard.getGrid()[0][0], testShip.getShipSections()[0]);
-
     }
 
     @Test
     public void testAddShipsPlacesSecondShipSectionOnBoardHorizontally(){
-        Board testBoard = new Board("test");
-        Ship testShip = new Ship("testShip", 3);
-
-        // ship, x-coord, y-coord
         testBoard.addShip(testShip, 0, 0 );
 
         assertEquals(testBoard.getSection(0, 1), testShip.getShipSection(1));
@@ -31,14 +30,37 @@ class BoardTest {
 
     @Test
     public void testAddShipsPlacesSecondShipSectionOnBoardVertically(){
-        Board testBoard = new Board("test");
-        Ship testShip = new Ship("testShip", 3);
-        testShip.rotate();
 
-        // ship, x-coord, y-coord
+        testShip.rotate();
         testBoard.addShip(testShip, 0, 0 );
 
         assertEquals(testBoard.getSection(1, 0), testShip.getShipSection(1));
+    }
 
+    @Test
+    void testGetGrid() {
+        assertEquals(testBoard.grid, testBoard.getGrid());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("TestBoard", testBoard.getName());
+    }
+
+    @Test
+    void testGetSection() {
+        assertEquals(testBoard.grid[0][0], testBoard.getSection(0,0));
+    }
+
+    @Test
+    void testAddSection() {
+        Sectionable testShipSection = new ShipSection("test");
+        testBoard.addSection(testShipSection, 0, 0);
+        assertEquals(testShipSection, testBoard.getSection(0,0));
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("Name: TestBoard" + ", grid: 10", testBoard.toString());
     }
 }
