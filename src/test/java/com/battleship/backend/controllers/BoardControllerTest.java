0eba9df572bed37e1e34834e7f4ea3d61414fb05
Lane Harris <lrharris215@gmail.com>
2021-12-02 14:57:47 -0500

    Test: Add tests for BoardRepository and PlaceRequest
    Getters and Setters, also changed the testBoardJSON order
    to match the attribute ordering in ShipSection.

--- src/test/java/com/battleship/backend/controllers/BoardControllerTest.java
+++ src/test/java/com/battleship/backend/controllers/BoardControllerTest.java
@@ -57,7 +57,7 @@ class BoardControllerTest {
                         .characterEncoding("UTF-8")
                         .content(getPlaceRequestInJSON(testShip, 0, 0));
 
-        String testBoardJSON = "{\"name\":\"testBoard\",\"grid\":[[{\"isHit\":false,\"isShip\":true,\"shipName\":\"test\"},{\"isHit\":false,\"isShip\":true,\"shipName\":\"test\"},{\"isHit\":false,\"isShip\":false}],[{\"isHit\":false,\"isShip\":false},{\"isHit\":false,\"isShip\":false},{\"isHit\":false,\"isShip\":false}],[{\"isHit\":false,\"isShip\":false},{\"isHit\":false,\"isShip\":false},{\"isHit\":false,\"isShip\":false}]]}";
+        String testBoardJSON = "{\"name\":\"testBoard\",\"grid\":[[{\"isShip\":true,\"isHit\":false,\"shipName\":\"test\"},{\"isShip\":true,\"isHit\":false,\"shipName\":\"test\"},{\"isHit\":false,\"isShip\":false}],[{\"isHit\":false,\"isShip\":false},{\"isHit\":false,\"isShip\":false},{\"isHit\":false,\"isShip\":false}],[{\"isHit\":false,\"isShip\":false},{\"isHit\":false,\"isShip\":false},{\"isHit\":false,\"isShip\":false}]]}";
 
         this.mockMvc.perform(builder)
                 .andExpect(MockMvcResultMatchers.status().isOk())
---
+++ src/test/java/com/battleship/backend/models/PlaceRequestTest.java
@@ -0,0 +1,30 @@
+package com.battleship.backend.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaceRequestTest {
+    PlaceRequest testRequest;
+    Ship testShip;
+    @BeforeEach
+    void initNewPlaceRequest(){
+        testShip = new Ship("TestShip", 2);
+        testRequest = new PlaceRequest(testShip, 0, 0);
+    }
+    @Test
+    void testGetShip() {
+        assertEquals(testShip, testRequest.getShip());
+    }
+
+    @Test
+    void testGetRow() {
+        assertEquals(0, testRequest.getRow());
+    }
+
+    @Test
+    void getCol() {
+        assertEquals(0, testRequest.getCol());
+    }
+}
