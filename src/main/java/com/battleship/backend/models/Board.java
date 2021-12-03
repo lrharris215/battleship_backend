79ef18d84b4929e3b69c942767d900ea30018c17
Lane Harris <lrharris215@gmail.com>
2021-12-03 14:33:22 -0500

    Refactor: Updated Board#setup
    Used addSection instead of indexing into grid.

--- src/main/java/com/battleship/backend/models/Board.java
+++ src/main/java/com/battleship/backend/models/Board.java
@@ -45,7 +45,7 @@ public class Board implements Boardable{
     private void setUp() {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                grid[i][j] = new NullSection();
+                addSection(new NullSection(), i, j);
             }
         }
     }
