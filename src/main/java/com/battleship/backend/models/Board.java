06850a933fefcb7e0a3fcfd71a17e6b36fafe7eb
Lane Harris <lrharris215@gmail.com>
2021-12-16 11:46:35 -0500

    Fix: Removed Extra Space

--- src/main/java/com/battleship/backend/models/Board.java
+++ src/main/java/com/battleship/backend/models/Board.java
@@ -73,7 +73,6 @@ public class Board implements Boardable{
                 if(boardShip.equals(otherShip)){
                     count += 1;
                 }
-
             }
         }
         return count == shipList.size();
